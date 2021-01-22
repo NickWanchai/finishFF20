0be5378d96c1aff0e641dd3a14243b6397501e6e
nickwanchai <wanchai93@hotmail.dk>
2021-01-22 11:19:42 +0100

    ny

--- NickVersion/src/test/java/ff20/demo/DemoApplicationTests.java
+++ NickVersion/src/test/java/ff20/demo/DemoApplicationTests.java
@@ -1,7 +1,7 @@
 package ff20.demo;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -9,6 +9,13 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
     }
+
+
+    @Test
+    void calculate(){
+        assertEquals(2, 4+1);
+    }
+
 
 
 }
